f42f341682e5a75caf5b0b0443ef65adfa306ce3
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2023-05-03 22:05:19 -0400

    Changed the location of the navbar.jsx file and took into the navbar folder.

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,7 +4,7 @@ import AuthPage from "../AuthPage/AuthPage";
 import NewOrderPage from "../NewOrderPage/NewOrderPage";
 import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
 import { Routes, Route } from "react-router-dom";
-import NavBar from "../../components/NavBar";
+import NavBar from "../../components/NavBar/NavBar";
 import { getUser } from "../../utilities/users-service";
 
 function App() {
